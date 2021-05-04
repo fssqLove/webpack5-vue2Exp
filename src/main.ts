0fb4ae0634b03df51c26e948c2d8797ed370bb6b
fssqLove <248120694@qq.com>
2021-05-04 15:53:40 +0800

    增加mobile和pcui库

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import './assets/font/iconfont.css'
 import router from './router/index'
 import store from './store'
 
+// 引入移动端ui库
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+// 引入pc端ui库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Vant)
+Vue.use(ElementUI)
+
 new Vue({
     router,
     store,
